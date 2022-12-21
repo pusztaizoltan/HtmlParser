714f46e0808a42a852e74fbb9717e9a458b428f3
pusztaizoltan <a.pusztaizoltan@gmail.com>
2022-12-21 11:25:24 +0100

    draft App to first test

--- src/main/java/com/training/htmlparser/App.java
+++ src/main/java/com/training/htmlparser/App.java
@@ -1,7 +1,10 @@
 package com.training.htmlparser;
 
+import com.training.htmlparser.model.HtmlFetcher;
+
 public class App {
 	public static void main(String[] args) {
-
+		HtmlFetcher fetcher = new HtmlFetcher("https://justinjackson.ca/words.html");
+		fetcher.fetch();
 	}
 }
