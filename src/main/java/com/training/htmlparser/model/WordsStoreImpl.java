bc5ae4879ca5885d2fbb8f10ac68b84f1b96456a
pusztaizoltan <a.pusztaizoltan@gmail.com>
2022-12-21 13:01:48 +0100

    implement WordsStore interface methods

--- src/main/java/com/training/htmlparser/model/WordsStoreImpl.java
+++ src/main/java/com/training/htmlparser/model/WordsStoreImpl.java
@@ -1,12 +1,32 @@
 package com.training.htmlparser.model;
 
-public class WordsStoreImpl implements WordsStore{
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class WordsStoreImpl implements WordsStore {
+	Set<String> skipWords = new HashSet<>();
+	Map<String, Integer> contentMap = new HashMap<>();
+
+	private boolean toSkip(String word) {
+		return skipWords.contains(word);
+	}
 
 	@Override
 	public void store(String word) {
+		String rawWord = word.toLowerCase();
+		if (!toSkip(rawWord)) {
+			if (contentMap.containsKey(rawWord)) {
+				contentMap.compute(word, (key, value) -> value++);
+			} else {
+				contentMap.put(word, 1);
+			}
+		}
 	}
 
 	@Override
 	public void addSkipWord(String word) {
+		skipWords.add(word.toLowerCase());
 	}
 }
