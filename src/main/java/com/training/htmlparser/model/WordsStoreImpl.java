48a0b417038c1ebfa979c662b170aabc6745e6e0
pusztaizoltan <a.pusztaizoltan@gmail.com>
2022-12-21 13:10:21 +0100

    implement getTenMostFrequent

--- src/main/java/com/training/htmlparser/model/WordsStoreImpl.java
+++ src/main/java/com/training/htmlparser/model/WordsStoreImpl.java
@@ -1,9 +1,12 @@
 package com.training.htmlparser.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class WordsStoreImpl implements WordsStore {
 	Set<String> skipWords = new HashSet<>();
@@ -28,5 +31,14 @@ public class WordsStoreImpl implements WordsStore {
 	@Override
 	public void addSkipWord(String word) {
 		skipWords.add(word.toLowerCase());
+	}
+
+	public List<String> getTenMostFrequent(){
+		return contentMap.entrySet()
+		                 .stream()
+		                 .sorted()
+		                 .limit(10)
+		                 .map(i->i.getKey())
+		                 .collect(Collectors.toList());
 	}
 }
