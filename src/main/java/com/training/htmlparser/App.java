590fe9827857c84db5386be1ab8e15b198cf3694
pusztaizoltan <a.pusztaizoltan@gmail.com>
2022-12-22 18:25:19 +0100

    implement Threadhandling Prototype

--- src/main/java/com/training/htmlparser/App.java
+++ src/main/java/com/training/htmlparser/App.java
@@ -5,11 +5,18 @@ import com.training.htmlparser.model.fetcher.JsoupFetcher;
 import com.training.htmlparser.model.wordsstore.WordsStore;
 import com.training.htmlparser.model.wordsstore.WordsStoreOfTenMostFrequentNonStream;
 
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 
 public class App {
+    static WordsStore wordsStore = new WordsStoreOfTenMostFrequentNonStream();
+    static List<String> urls = List.of("https://justinjackson.ca/words.html");
+
     static void setGlobalLoggerVisibility(Level level) {
         for (Handler handler : LogManager.getLogManager().getLogger("").getHandlers()) {
             handler.setLevel(level);
@@ -18,17 +25,34 @@ public class App {
 
     public static void main(String[] args) {
         setGlobalLoggerVisibility(Level.SEVERE);
-        // outOfThread
-        WordsStore wordsStore = new WordsStoreOfTenMostFrequentNonStream();
-        // inThread
-//        Fetcher fetcher = new HtmlFetcher("https://justinjackson.ca/words.html");
-        Fetcher fetcher = new JsoupFetcher("https://justinjackson.ca/words.html");
-        fetcher.addSkipTag("head");
-        fetcher.addSkipTag("style");
-        fetcher.processWordContent();
-        for (String word : fetcher.getWordContent()) {
-            wordsStore.store(word);
+        ExecutorService executor = Executors.newCachedThreadPool();
+        for (String url : urls) {
+            executor.execute(fetcherThread(url));
+        }
+        executor.shutdown();
+        try {
+            executor.awaitTermination(120, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         wordsStore.select().forEach(System.out::println);
     }
+
+    public static Runnable fetcherThread(String url) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                Fetcher fetcher = new JsoupFetcher(url);
+                fetcher.addSkipTag("head");
+                fetcher.addSkipTag("style");
+                fetcher.processWordContent();
+                for (String word : fetcher.getWordContent()) {
+                    wordsStore.store(word);
+                }
+            }
+        };
+    }
 }
+
+
+
