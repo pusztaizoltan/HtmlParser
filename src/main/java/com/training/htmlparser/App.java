460ef85e3ed8c34fcf8e302de47d75d0217adadd
pusztaizoltan <a.pusztaizoltan@gmail.com>
2022-12-23 15:14:06 +0100

    Add logFormat prototype

--- src/main/java/com/training/htmlparser/App.java
+++ src/main/java/com/training/htmlparser/App.java
@@ -5,26 +5,38 @@ import com.training.htmlparser.model.fetcher.JsoupFetcher;
 import com.training.htmlparser.model.wordsstore.WordsStore;
 import com.training.htmlparser.model.wordsstore.selectoralgorithms.UniqueLengthOrderer;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Formatter;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
+import java.util.logging.LogRecord;
 
 public class App {
+    static Formatter customLogFormatter = new Formatter() {
+        @Override
+        public String format(LogRecord record) {
+            return LocalDateTime.now() + " Thread:" + record.getThreadID() + " Level:" + record.getLevel() + " Message:" + record.getMessage() + "\n";
+        }
+    };
+
     static WordsStore wordsStore = new WordsStore(new UniqueLengthOrderer());
     static List<String> urls = List.of("https://justinjackson.ca/words.html");
 
     static void setGlobalLoggerVisibility(Level level) {
         for (Handler handler : LogManager.getLogManager().getLogger("").getHandlers()) {
             handler.setLevel(level);
+            handler.setFormatter(customLogFormatter);
         }
     }
 
     public static void main(String[] args) {
-        setGlobalLoggerVisibility(Level.SEVERE);
+        setGlobalLoggerVisibility(Level.INFO);
         ExecutorService executor = Executors.newCachedThreadPool();
         for (String url : urls) {
             executor.execute(fetcherThread(url));
@@ -47,6 +59,7 @@ public class App {
             wordsStore.storeAll(fetcher.getWordContent());
         };
     }
+
 }
 
 
